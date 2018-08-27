f9f1da48a8c140f88eea139d362198c4858b833d
Josue Caraballo <josue.caraballo@gmail.com>
2018-08-27 18:02:51 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure  => directory,
+  }
+  file {'/opt/minecraft/server.jar':
+    ensure  => file,
+    source  => 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
+  }
+  package {'java':
+    ensure  => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure  => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure  => file,
+    source  => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
