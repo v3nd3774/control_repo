30e1d8eddb5cde05a9d5c45f2e64bf9531a271c6
Josue Caraballo <josue.caraballo@gmail.com>
2018-08-27 15:45:12 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,4 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a README!',
-    owner   => 'root',
-  }
 }
 node 'master.puppet.vm' {
   include role::master_server
