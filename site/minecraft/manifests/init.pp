10f1c3ec1cbec3ca0e1328d0c3781a791e017cae
Josue Caraballo <josue.caraballo@gmail.com>
2018-08-27 18:23:14 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,5 @@
 class minecraft (
-  $url = 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar'
+  $url = 'https://launcher.mojang.com/mc/game/1.13.1/server/fe123682e9cb30031eae351764f653500b7396c9/server.jar',
   $install_dir = '/opt/minecraft'
 ){
   file {$install_dir:
