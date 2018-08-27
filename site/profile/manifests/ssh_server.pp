1e89b1a575240ee69e016f78a0f8910bb4cdf3f1
Josue Caraballo <josue.caraballo@gmail.com>
2018-08-27 16:36:37 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure  => present,
     user    => 'root',
     type    => 'ssh-rsa',
-    key     => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJsdvRm5w6U+c26iWMusZnQ/FyCuRGLZftr9R0jXBTS0oXIC0wfBXqdwcOzGeAmzrzrhxlSkFY3eMTlq/XYtpl+YL7rzYSta3ADZfn3vfuRc9NGDxISyCQo/CibeH+VP/IV2MVd5t2O+os6XgQUiOUfQquyQZRyx6RC7JVu5V9EL/hdFgVHuNRQTWZgj8t71No2EuKFsbsascaL6tW+Mw5/8KDOLieiwvTY4DL5jQpv+hPZFJ/rxSvsCzIIfow/P49RyFd75ZtXwEi2h0mIlB4zsSMUY05iiwlOBZ7R9C0HXnDGcY0e+tBmiXo2+nq16QXMA7DnktUsjszFEvMMHNn',
+    key     => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDJsdvRm5w6U+c26iWMusZnQ/FyCuRGLZftr9R0jXBTS0oXIC0wfBXqdwcOzGeAmzrzrhxlSkFY3eMTlq/XYtpl+YL7rzYSta3ADZfn3vfuRc9NGDxISyCQo/CibeH+VP/IV2MVd5t2O+os6XgQUiOUfQquyQZRyx6RC7JVu5V9EL/hdFgVHuNRQTWZgj8t71No2EuKFsbsascaL6tW+Mw5/8KDOLieiwvTY4DL5jQpv+hPZFJ/rxSvsCzIIfow/P49RyFd75ZtXwEi2h0mIlB4zsSMUY05iiwlOBZ7R9C0HXnDGcY0e+tBmiXo2+nq16QXMA7DnktUsjszFEvMMHNn',
   }
 }
